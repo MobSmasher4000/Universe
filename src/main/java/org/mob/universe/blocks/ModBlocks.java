ae98f3b3df9d1accb7c232b1bf2209302763bcec
Zombiebear99 <dsehrawat521+mobsmasher4000+github@gmail.com>
2025-10-20 11:14:38 +0530

    switched to parchment mappings.

--- src/main/java/org/mob/universe/blocks/ModBlocks.java
+++ src/main/java/org/mob/universe/blocks/ModBlocks.java
@@ -11,7 +11,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.mob.universe.Universe;
 import org.mob.universe.blocks.custom.UniverseBlock;
-import org.zombie.universe.blocks.custom.*;
 import org.mob.universe.item.ModItems;
 
 import java.util.function.Supplier;
